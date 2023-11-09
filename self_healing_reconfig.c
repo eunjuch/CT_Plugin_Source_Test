fd05b61edea68d6c8bf2822874c5f785409c627e
eunjuch <juicy123123@naver.com>
2023-11-09 11:38:13 +0900

    self healing code

---
+++ self_healing_reconfig.c
@@ -0,0 +1,19 @@
+int func2(int a) {
+	a += 2;
+	return 0;
+}
+
+int function2(int a2, int b2) {
+	a2 += func2(a2);
+	return 0;
+}
+
+int function3(int a3, int b3, int c3) {
+	a3 += func2(a3);
+	return 0;
+}
+
+int function4(int a4, int b4, int c4, int d4) {
+	a4 += func2(a4);
+	return 0;
+}
